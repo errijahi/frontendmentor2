e32482ec0d4351671ff16a4d9ff9aa2f9fb2c050
errijahi <76177230+errijahi@users.noreply.github.com>
2021-06-10 11:19:16 +0000

    Add files via upload

---
+++ faq-accordion-card-main/text.js
@@ -0,0 +1,79 @@
+function myFunction() {
+    var icon = document.getElementById("icon");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (icon.style.transform === "rotate(180deg)") {
+        icon.style.transform = "rotate(0deg)";
+        moreText.style.display = "none";
+        btnText.style.fontWeight = "400";
+    } else {
+        icon.style.transform = "rotate(180deg)";
+        moreText.style.display = "inline";
+        btnText.style.fontWeight = "700";
+    }
+}
+
+function myFunction2() {
+    var icon = document.getElementById("icon2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+
+    if (icon.style.transform === "rotate(180deg)") {
+        icon.style.transform = "rotate(0deg)";
+        moreText.style.display = "none";
+        btnText.style.fontWeight = "400";
+    } else {
+        icon.style.transform = "rotate(180deg)";
+        moreText.style.display = "inline";
+        btnText.style.fontWeight = "700";
+    }
+}
+
+function myFunction3() {
+    var icon = document.getElementById("icon3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+
+    if (icon.style.transform === "rotate(180deg)") {
+        icon.style.transform = "rotate(0deg)";
+        moreText.style.display = "none";
+        btnText.style.fontWeight = "400";
+    } else {
+        icon.style.transform = "rotate(180deg)";
+        moreText.style.display = "inline";
+        btnText.style.fontWeight = "700";
+    }
+}
+
+function myFunction4() {
+    var icon = document.getElementById("icon4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+
+    if (icon.style.transform === "rotate(180deg)") {
+        icon.style.transform = "rotate(0deg)";
+        moreText.style.display = "none";
+        btnText.style.fontWeight = "400";
+    } else {
+        icon.style.transform = "rotate(180deg)";
+        moreText.style.display = "inline";
+        btnText.style.fontWeight = "700";
+    }
+}
+
+function myFunction5() {
+    var icon = document.getElementById("icon5");
+    var moreText = document.getElementById("more5");
+    var btnText = document.getElementById("myBtn5");
+
+    if (icon.style.transform === "rotate(180deg)") {
+        icon.style.transform = "rotate(0deg)";
+        moreText.style.display = "none";
+        btnText.style.fontWeight = "400";
+    } else {
+        icon.style.transform = "rotate(180deg)";
+        moreText.style.display = "inline";
+        btnText.style.fontWeight = "700";
+    }
+}
